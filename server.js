459cad8874a6b1c1b867feee5427bf4e61e5e670
Derek Wheelden <derek.wheelden@gmail.com>
2018-12-19 21:01:52 -0500

    Add body-parser

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const newCustomer = require('./newCustomer');
 
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.status(401).send('You shouldn\'t be here'));
 
 app.post('/newcustomer', function (req, res) {
-    const success = newCustomer(res.body);
+    const success = newCustomer(req.body);
     res.status(success ? 200 : 500).send();
 });
 
