9a2364a444634b89b75e43242c432e01d4525823
Derek Wheelden <derek.wheelden@gmail.com>
2018-12-19 20:57:26 -0500

    You must send the response

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const newCustomer = require('./newCustomer');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.status(401));
+app.get('/', (req, res) => res.status(401).send('You shouldn\'t be here'));
 
 app.post('/newcustomer', function (req, res) {
     const success = newCustomer(res.body);
-    res.status(success ? 200 : 500);
+    res.status(success ? 200 : 500).send();
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
